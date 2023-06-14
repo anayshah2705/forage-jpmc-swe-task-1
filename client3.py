52fd8a4c8a3761764ebbfe4136ceb29706a3f440
anayshah2705 <117211606+anayshah2705@users.noreply.github.com>
2023-06-14 09:50:52 +0530

    Update client3.py

--- client3.py
+++ client3.py
@@ -40,8 +40,7 @@ def getDataPoint(quote):
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
+    ratio = float(price_a/price_b)
     return 1
 
 
